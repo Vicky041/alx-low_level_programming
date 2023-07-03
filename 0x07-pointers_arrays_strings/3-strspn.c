4c68ad17fb6aa4875151e6762119365a37a32b45
Vicky041 <osemekev22@gmail.com>
2023-07-03 16:20:49 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,11 +13,15 @@ for (i = 0; s[i] != '\0'; i++)
 {
 check = 0;
 for (n = 0; accept[n] != '\0'; n++)
+{
 if (accept[n] == s[i])
 {
 value++;
 check = i;
 }
 }
-return (0);
+if (check != i)
+break;
+}
+return (value);
 }
